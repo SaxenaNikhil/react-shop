88ecb8998331051010839e471dea1f2c7c5c5b57
Nikhil Saxena <nikhilsaxena003@gmail.com>
2023-12-04 23:54:40 +0530

    Creating Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Navbar from "./components/Navbar";
+
 export default function App() {
   return (
     <>
+      <Navbar/>
       <h1>Hello World</h1>
       <p>Welcome to the course!</p>
       <i className="bi bi-activity"></i>
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,6 @@
+//a function is a component which returns JSX
+export default function Navbar () {
+    return (
+        <h1>This is Navbar!</h1>
+    )
+}
